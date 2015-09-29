e009eabab596eeb2201e9902a6f0386b38b022f7
Cathy <cathy@schoolchapters.com>
2015-09-29 17:42:08 +0000

    restricting access only authorized users can delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.order(:name).page params[:page]
@@ -32,7 +32,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render :text => 'Not allowed', :status => :forbidden
 		end
-		
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 
@@ -41,6 +41,10 @@ class PlacesController < ApplicationController
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
+		
 		@place.destroy
 		redirect_to root_path
 	end
